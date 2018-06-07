8e0d9967d5539678485833a2526adc412c81343f
Yaduraj Shakti <P7109748@ntil.com>
2018-06-07 20:03:00 +0530

    added src

---
+++ src/CalculatorApp/CalculatorApp.Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorApp;
+
+namespace CalculatorApp.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var result = Program.Add(1, 2);
+
+            Assert.AreEqual(3, result);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            var result = Program.Multi(3, 2);
+
+            Assert.AreEqual(6, result);
+
+        }
+    }
+}
---
+++ src/CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Multi(3, 2));
+
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Multi(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
